e05c4ec0cf248cbb31690f0676aa538eb4e1b4d7
chugaev-sp <chugaev-sp@bisys.ru>
2017-03-15 17:57:30 +0500

    git angular sign form

--- Angular4/payments-app/src/app/app.component.ts
+++ Angular4/payments-app/src/app/app.component.ts
@@ -31,7 +31,7 @@ import { SignService, User, } from './sign.service'
 export class AppComponent implements OnInit {
   users: User[];
   submitted: boolean = false;
-  isPaymets: boolean = true;
+  isPaymets: boolean = false;
   usr: string;
   pass: string;
 
