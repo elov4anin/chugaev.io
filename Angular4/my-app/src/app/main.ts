0e5593db7950f3a23c2172532b8de498e5dec84c
Sergey Chugaev <elov4anin@gmail.com>
2017-04-18 23:38:35 +0600

    vk api friends.get

--- Angular4/my-app/src/app/main.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by user on 3/1/17.
- */
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
--- Angular4/my-app/src/systemjs.config.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by user on 3/2/17.
- */
