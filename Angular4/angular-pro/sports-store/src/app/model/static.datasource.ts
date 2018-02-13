164cdabe9fcb49f9ceca66d23a9d2fa8240794db
chugaev-sp <chugaev-sp@bisys.ru>
2018-02-13 15:20:27 +0500

    angular pro sports store end v1

--- Angular4/angular-pro/sports-store/src/app/model/static.datasource.ts
+++ Angular4/angular-pro/sports-store/src/app/model/static.datasource.ts
@@ -1,7 +1,10 @@
 import { Injectable } from "@angular/core";
-import { Product } from "./product.model";
+
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/observable/from";
+
+import { Product } from "./product.model";
+import { Order } from "./order.model";
 
 @Injectable()
 
@@ -25,5 +28,10 @@ export class StaticDataSource {
 ];
   getProducts(): Observable<Product[]> {
     return Observable.from([this.products]);
+  }
+
+  saveOrder(order: Order): Observable<Order> {
+    console.log(JSON.stringify(order));
+    return Observable.from([order]);
   }
 }
