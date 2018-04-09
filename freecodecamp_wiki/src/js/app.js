3b4a2b757227e5ffc7bb8dac7b064ab0d0e3c715
chugaev-sp <elov4anin@gmail.com>
2018-04-09 21:49:43 +0500

    test new build project

--- freecodecamp_wiki/src/js/app.js
+++ freecodecamp_wiki/src/js/app.js
@@ -1,1 +1,26 @@
 //it is js script!
+import  $ from 'jquery';
+
+$(document).ready(()=> {
+    $.ajax({
+        url: "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&formatversion=2",
+        /*url: "/ck-landings/Operators/php/callback.php",*/
+        /*url: "/lp/test/taxi/php/callback.php",*/
+
+        type: "GET",
+        dataType: 'json',
+
+        success: function (dat) {
+            console.log(dat)
+
+
+
+        },
+        error: function(E){
+           console.log(e)
+        },
+        complete: function(){
+
+        }
+    });
+});
